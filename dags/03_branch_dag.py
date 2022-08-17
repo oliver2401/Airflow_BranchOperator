a3a273073396019ff42a34f3334094eb9638f2ba
oliver2401 <oliver.1012@hotmail.com>
2022-08-16 19:36:37 -0500

    Changed ERP name to DATA source

--- dags/03_branch_dag.py
+++ dags/03_branch_dag.py
@@ -3,11 +3,11 @@ from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 
-ERP_CHANGE_DATE = airflow.utils.dates.days_ago(1)
+DATA_SOURCE_DATE = airflow.utils.dates.days_ago(1)
 
 
 def _pick_data_source(**context):
-    if context["execution_date"] < ERP_CHANGE_DATE:
+    if context["execution_date"] < DATA_SOURCE_DATE:
         return "fetch_sales_old"
     else:
         return "fetch_sales_new"
